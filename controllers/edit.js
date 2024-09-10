306d004b0a3ce8821d516423e4462f69219b6104
Carolyn Wilson <carolynw0902@gmail.com>
2024-09-10 16:17:09 -0400

    added an edit.js to the controllers folder

---
+++ controllers/edit.js
@@ -0,0 +1,22 @@
+const ItemList = require('../models/itemList')
+
+module.exports = {
+  getEdit: async (req,res) => {
+      const id = req.params.id
+      console.log(id)
+      try {
+        const items = await ItemList.find()
+        res.render("edit.ejs", { itemList: items, idItem: id})
+      } catch (err){
+        if(err) return res.status(500).send(err)
+      }
+  },
+
+  deleteItem: async (req,res) => {
+
+  },
+
+  updateItem: async (req,res) => {
+
+  }
+}
